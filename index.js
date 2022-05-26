eb42939bed7e3b6cf265ebf4b05f79d0868ed00f
PanduAldi <pratamapandu652@gmail.com>
2022-05-26 10:09:18 +0700

    Tantangan Struktur Data Pertemuan 5 - commit ke-enam : Done

--- index.js
+++ index.js
@@ -2,51 +2,13 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
-/** Color data */
-const yellow = "Yellow";
-const pink = "Pink";
-const white = "White";
-const purple = "Purple";
-const blue = "Blue";
-const black = "Black";
-const grey = "Grey";
-
-/** Resto data */
-const bento = "Bento";
-const sushi = "Sushi";
-const pancake = "Pancake";
-const eggy = "Eggy";
-const tempura = "Tempura";
-const padang = "Padang";
-const tteok = "Tteok";
-const katsu = "Katsu";
-const geprek = "Geprek";
-
 /** todo Monica property */
 const arrColorMonica = ["Yellow", "Pink", "White", "Purple"];
 const arrRestoMonica = ["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]; 
 
 let setColorMonica = new Set(arrColorMonica);
-// setColorMonica.add(yellow);
-// setColorMonica.add(pink);
-// setColorMonica.add(white);
-// setColorMonica.add(purple);
-
-
 let setRestoMonica = new Set(arrRestoMonica);
-// setRestoMonica.add(bento);
-// setRestoMonica.add(sushi);
-// setRestoMonica.add(pancake);
-// setRestoMonica.add(eggy);
-// setRestoMonica.add(tempura);
-// setRestoMonica.add(bento);
-// setRestoMonica.add(eggy);
-// setRestoMonica.add(padang);
-// setRestoMonica.add(tteok);
-// setRestoMonica.add(sushi);
-// setRestoMonica.add(sushi);
 
- 
 const educationMonica = [
     { name: "SD 01", city: "Jakarta", graduate: "2016" },
     { name: "SMP 02", city: "Jakarta", graduate: "2019" },
@@ -58,21 +20,7 @@ const arrColorWendy = ["Blue", "Black", "Grey"];
 const arrRestoWendy = ["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]; 
 
 let setColorWendy = new Set(arrColorWendy);
-// setColorWendy.add(blue);
-// setColorWendy.add(black);
-// setColorWendy.add(grey);
-
-
 let setRestoWendy = new Set(arrRestoWendy);
-// setRestoWendy.add(tempura);
-// setRestoWendy.add(bento);
-// setRestoWendy.add(sushi);
-// setRestoWendy.add(pancake);
-// setRestoWendy.add(padang);
-// setRestoWendy.add(katsu);
-// setRestoWendy.add(geprek);
-// setRestoWendy.add(pancake);
-// setRestoWendy.add(eggy);
 
 
 const educationWendy = [
