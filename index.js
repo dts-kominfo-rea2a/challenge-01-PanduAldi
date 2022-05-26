58b21679fbc6df4cd4d17f3e2730494a972e485a
PanduAldi <pratamapandu652@gmail.com>
2022-05-26 09:24:25 +0700

    Tantangan Struktur Data Pertemuan 5 - commit kedua

--- index.js
+++ index.js
@@ -37,10 +37,10 @@ const firstUser = {
     gender: "Female",
     age: 17,
     email: "monica@dingdong.com",
-    favoriteColor: setColorMonica,
+    favoriteColor: setColorMonica.values(),
     isHavePet: "Yes",
     education: educationMonica,
-    favoriteRestaurant: setRestoMonica
+    favoriteRestaurant: setRestoMonica.values()
 };
 
 const secondUser = {
